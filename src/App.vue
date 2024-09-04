9ef55021f545b09b3c70a0a13f57fb05aa0ef012
荒 <1207588603@qq.com>
2024-09-04 17:12:55 +0800

    重构：不使用孤立分支

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <h1>欢迎来到我的朝花夕拾</h1>
-
-  <div class="mb-4">
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
-  </div>
+  <div class="app-content"></div>
 </template>
 
 <script setup lang="ts">
