70209e64ff3ef8be10364be7ed4064301c8a7984
荒 <1207588603@qq.com>
2024-09-04 13:22:14 +0800

    新增：自动按需引入 ElementPlus 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>欢迎来到我的朝花夕拾</h1>
+
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <script setup lang="ts">
