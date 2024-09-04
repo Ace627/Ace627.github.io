2e75d1e78374d4ec2086aaabc7eb8e69ef92b70f
荒 <1207588603@qq.com>
2024-09-04 10:50:57 +0800

    新增：项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>欢迎来到我的朝花夕拾</h1>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'App' })
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  padding-top: 100px;
+  font-size: 36px;
+  text-align: center;
+}
+</style>
