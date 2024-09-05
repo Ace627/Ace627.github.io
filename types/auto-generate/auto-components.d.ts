020323f51a917122062b6d6fb04b1bc5a828fee4
荒 <1207588603@qq.com>
2024-09-05 09:54:44 +0800

    重构：git 忽略文件配置

--- types/auto-generate/auto-components.d.ts
+++ types/auto-generate/auto-components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElLink: typeof import('element-plus/es')['ElLink']
-    ElText: typeof import('element-plus/es')['ElText']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
