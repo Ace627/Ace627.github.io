50e22831cf9c5ec6b210c592db079263b57dc0ed
当时只道是寻常 <1207588603@qq.com>
2024-10-06 12:36:59 +0800

    perf: 代码格式化工具 Prettier 配置改为带注释版本

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="layout-common clearFix" :class="classes">
-    <LayoutClassic v-if="settingStore.layout === 'classic'" />
-    <LayoutTop v-else-if="settingStore.layout === 'top'" />
-  </div>
+	<div class="layout-common clearFix" :class="classes">
+		<LayoutClassic v-if="settingStore.layout === 'classic'" />
+		<LayoutTop v-else-if="settingStore.layout === 'top'" />
+	</div>
 
-  <!-- 应用配置面板 -->
-  <SettingPanel />
+	<!-- 应用配置面板 -->
+	<SettingPanel />
 </template>
 
 <script setup lang="ts">
@@ -20,11 +20,11 @@ const settingStore = useSettingStore()
 
 /** 用来添加到根组件的动态类的集合 */
 const classes = computed(() => [
-  appStore.device,
-  `layout-${settingStore.layout}`,
-  { 'open-sidebar': appStore.sidebar.opened },
-  { 'hide-sidebar': !appStore.sidebar.opened },
-  { withoutAnimation: appStore.sidebar.withoutAnimation },
+	appStore.device,
+	`layout-${settingStore.layout}`,
+	{ 'open-sidebar': appStore.sidebar.opened },
+	{ 'hide-sidebar': !appStore.sidebar.opened },
+	{ withoutAnimation: appStore.sidebar.withoutAnimation }
 ])
 </script>
 
