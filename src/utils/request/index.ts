5f1ce38c3c16a01cd352244587bee8b77de2aa22
当时只道是寻常 <1207588603@qq.com>
2024-10-06 12:45:31 +0800

    feat: 根据响应异常时的 Error 提取解析出符合直觉的 Message

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { HttpStatusEnum, RequestMethod } from '@/enums'
-import { handleErrorCode } from './status-code'
+import { handleResponseError } from './helper'
 import { getAccessToken } from '@/utils/cache/local-storage'
 
 const { VITE_BASE_API, VITE_REQUEST_TIMEOUT, VITE_REQUEST_NPROGRESS } = useEnv() // 解构环境变量
@@ -30,7 +30,9 @@ request.interceptors.request.use(
     return config
   },
   (error) => {
-    NProgress.done() // 关闭响应进度条
+    // 关闭响应进度条
+    NProgress.done()
+    // 将原初异常以 Promise 形式暴露给外部调用处理
     return Promise.reject(error)
   },
 )
@@ -38,7 +40,7 @@ request.interceptors.request.use(
 // 响应拦截器
 request.interceptors.response.use(
   (response) => {
-    NProgress.done() // 关闭响应进度条
+    NProgress.done()
     const withoutTransformResponse = response.config.headers['withoutTransformResponse'] === false // 是否返回原生 response.data
     const isBinary = ['blob', 'arraybuffer'].includes(response.request.responseType) // 是否二进制数据
     if (withoutTransformResponse || isBinary) return response.data
@@ -51,11 +53,8 @@ request.interceptors.response.use(
     return Promise.reject(new Error(message))
   },
   (error: any) => {
-    NProgress.done() // 关闭响应进度条
-    let message = error.message
-    if (error.response) message = handleErrorCode(error.response.status)
-    console.log('响应拦截器异常: ', message || error.message || error) // for debug
-    alert(message) // 此处可统一处理错误的提示消息
+    NProgress.done()
+    handleResponseError(error) // 根据响应异常时的 Error 提取解析出符合直觉的 Message
     return Promise.reject(error)
   },
 )
